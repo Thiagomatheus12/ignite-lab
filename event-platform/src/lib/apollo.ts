4a85f24b8a8368055d7006ffd1ae0dc5d5758dd5
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-06-22 16:37:59 -0300

    aula 02 Ignite Lab

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient ({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4o9wqb01imq01xxbhb4edpl/master",
+    uri: "https://api-sa-east-1.graphcms.com/v2/cl4prtiaf2kf501z48y7k27ly/master",
     cache: new InMemoryCache()
 })
